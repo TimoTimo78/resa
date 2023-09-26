8c046de796f4b4e8de19c937a1d7962bc3a5d2b4
Cedric <116574782+TimoTimo78@users.noreply.github.com>
2023-09-26 13:45:30 +0200

    First commit

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,8 +2,10 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://www.dsi-ap.com/site_dsi')
     // cy.get('a[href*="/site_dsi/vos-avantages.html"]').first().click()
+    // Pour faire un test du click sur "Vos avantages"
     cy.get('a').contains('Vos avantages').click() // Yield el in .nav containing 'About'
     // cy.contains('Hello') // Yield first el in document containing 'Hello'
+    // Pour effcetuer un test de l'url
     cy.url('https://www.dsi-ap.com/site_dsi/vos-avantages.html').should('contain', 'https://www.dsi-ap.com/site_dsi/vos-avantages.html') // Yields the current URL as a string
   })
 })
